65beab7059c0938c899c5ee6cee303e81b8ee16b
Mostafa Farzan <farzan@cakerobotics.com>
2021-11-25 15:43:29 +0330

    Allow updating config based on  world/robot

--- launcher/config/collect.py
+++ launcher/config/collect.py
@@ -2,21 +2,46 @@ from os import environ
 import os.path
 import yaml
 
+from ..find_default_asset_path import find_robot_path, find_world_path
+
 # This function collects simulation configuration from the following sources
 # (the later sources override previous ones):
-#   1. default sim config
-#   2. /sim/config.yaml
-#   3. ${cwd}/config.yaml
-#   4. environment variables
+#   1. robot_directory/config.yaml
+#   2. world_directory/config.yaml
+#   3. default sim config
+#   4. /sim/config.yaml
+#   5. ${cwd}/config.yaml
+#   6. environment variables
 # The structure is described in TODO.html
 #
 def collect_config():
     config = {}
+
+    # Load default config
     config.update(load_default_config())
+
+    # Load the config specified by user
     config.update(load_yaml_config('/sim/config.yaml'))
     config.update(load_yaml_config('./config.yaml'))
     config.update(load_env_config())
+
+    # Load config specified by robot
+    robot_dir = os.path.dirname(find_robot_path(config['robot']))
+    robot_config_path = os.path.join(robot_dir, 'config.yaml')
+    config.update(load_yaml_config(robot_config_path))
+
+    # Load config specified by world
+    world_dir = os.path.dirname(find_world_path(config['world']))
+    world_config_path = os.path.join(world_dir, 'config.yaml')
+    config.update(load_yaml_config(world_config_path))
+
+    # Load the config specified by user AGAIN (To override everything if needed)
+    config.update(load_yaml_config('/sim/config.yaml'))
+    config.update(load_yaml_config('./config.yaml'))
+    config.update(load_env_config())
+
     return config
+
 
 def load_default_config():
     module_dir = os.path.dirname(os.path.realpath(__file__))
